18ace374526eddfa749fe09e02719326b466995e
Onur ÖTLEŞ <onurotles@gmail.com>
2025-09-02 00:02:23 +0300

    localhosta geri çekildi test

--- src/push.ts
+++ src/push.ts
@@ -19,7 +19,7 @@ export async function subscribeUser() {
     });
 
     // Backend’e gönder
-    await fetch('http://countdown-push-server.onrender.com/subscribe', {
+    await fetch('http://localhost:4000/subscribe', {
       method: 'POST',
       body: JSON.stringify(subscription),
       headers: { 'Content-Type': 'application/json' },
