7f665fcfe0d7fbe838bf268592fef6a62b98bd22
Onur ÖTLEŞ <onurotles@gmail.com>
2025-09-02 00:04:35 +0300

    homepage

--- src/push.ts
+++ src/push.ts
@@ -19,7 +19,7 @@ export async function subscribeUser() {
     });
 
     // Backend’e gönder
-    await fetch('http://localhost:4000/subscribe', {
+    await fetch('http://countdown-push-server.onrender.com/subscribe', {
       method: 'POST',
       body: JSON.stringify(subscription),
       headers: { 'Content-Type': 'application/json' },
